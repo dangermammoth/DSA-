02cdb9e0745e850e7422d5cd6ea20282c277b70e
dangermammoth <kmaheshwari679@gmail.com>
2026-07-13 09:11:54 +0530

    Time: 0 ms (100%), Space: 17.3 MB (15.82%) - LeetHub

---
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+         vector<vector<int>>res;
+        if(root==nullptr) return res;
+        queue<TreeNode*>q;
+        q.push(root);
+
+       
+        while(!q.empty()){
+            int n=q.size();
+            vector<int>ans;
+            for(int i=0;i<n;i++){
+                TreeNode*curr=q.front();
+                q.pop();
+                ans.push_back(curr->val);
+
+                if(curr->left)q.push(curr->left);
+                if(curr->right)q.push(curr->right);
+            }
+            res.push_back(ans);
+            
+        }
+        return res;
+    }
+};
